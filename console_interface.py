a4a601431bd4105e7de7ae64578bffb8c6dbd69b
Khilya Dmitry <demonukraine96@gmail.com>
2017-08-24 01:27:09 +0300

    Added readme.md

--- console_interface.py
+++ console_interface.py
@@ -6,23 +6,20 @@ def cls():
     os.system('cls' if os.name=='nt' else 'clear')
 
 def main():
-    search_string = input('Введите адрес:\n')
+    search_string = input('Input address:\n')
     cls()
 
-    places = [DestinationPlace('Шота Руставели 27Б, Киев', 'Работа'),
-              DestinationPlace('станция метро Академгородок', 'Академгородок'),
-              DestinationPlace('Богдановская 10, Киев', 'Пред. дом (богдановская 10)'),
-              DestinationPlace('станция метро Вокзальная', 'Вокзальная'),]
+    places = [DestinationPlace('Someone Adrres, 77a', 'Work'),]
     
     loop = asyncio.get_event_loop()
     api = AddressSearchAPI()
 
     place = loop.run_until_complete(api.get_place_by_search_string(search_string))
     place = loop.run_until_complete(api.get_nearest_subway_station(place))
-    print('Искомое место: {}'.format(place.address))
-    print('Ближайшая стацния метро:\n\n{}, {} пешком\n'.format(place.nearest_subway_station.name, place.distance_to_nearest_subway_station))
+    print('Searching address: {}'.format(place.address))
+    print('Nearest subway station:\n\n{}, {} walking\n'.format(place.nearest_subway_station.name, place.distance_to_nearest_subway_station))
 
-    print('Время до мест:')
+    print('Movement time:')
     for place_index in range(len(places)):
         places[place_index] = loop.run_until_complete(api.get_general_info_by_search_string(places[place_index]))
         places[place_index].distance = loop.run_until_complete(api.get_destantion(place, places[place_index], places[place_index].movement_type))
---
+++ readme.md
@@ -0,0 +1,2 @@
+Usage:
+    Setup client using AddressSearchAPI. First parameter must be api key of Google Maps API
